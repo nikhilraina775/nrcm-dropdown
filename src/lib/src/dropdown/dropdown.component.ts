2ae2c9bf55d774843bc76afddde00abc7a8687a9
Nikhil__Raina <nikhil.raina@hashworks.com>
2018-04-26 16:44:41 +0530

    close on clicking outside

--- src/lib/src/dropdown/dropdown.component.ts
+++ src/lib/src/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter, HostListener, ElementRef } from '@angular/core';
 // import { EventEmitter } from 'protractor';
 
 @Component({
@@ -8,13 +8,14 @@ import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 })
 export class DropdownComponent implements OnInit {
   selectedOptionValue: string;
-  dropdownList:boolean=false;
+  dropdownList =false;
 
 
-@Input() addDropdownClass:string="kjwsvh";
-@Input() options=[{
+@Input() addDropdownClass ='kjwsvh';
+@Input() options = [{
   label: 'option 1',
-  value: 'value 1'
+  value:
+  'value 1'
 }, {
   label: 'option 2',
   value: 'value 2'
@@ -28,20 +29,35 @@ export class DropdownComponent implements OnInit {
   label: 'option 5',
   value: 'value 5'
 }];
-@Input() selectedOption:string="Select an option";
-@Output() 
-changeDropdown= new EventEmitter();
+@Input() selectedOption = 'Select an option';
+@Output()
+changeDropdown = new EventEmitter();
 
+public text: String;
 
-  constructor() { }
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    debugger;
+    if(this.eRef.nativeElement.contains(event.target)) {
+      this.text = 'clicked inside';
+    } else {
+      this.text = 'clicked outside';
 
-  selectAnOption(){
-    this.dropdownList= !this.dropdownList;
+      this.dropdownList = false;
+    }
   }
-  onClickOption(value){
+
+  constructor(private eRef: ElementRef) {
+    this.text = 'no clicks yet';
+  }
+
+  selectAnOption() {
+    this.dropdownList = !this.dropdownList;
+  }
+  onClickOption(value) {
     this.selectedOption = this.options[value].label;
     this.selectedOptionValue = this.options[value].value;
-    this.dropdownList= false;
+    this.dropdownList = false;
     this.changeDropdown.emit(this.selectedOptionValue);
   }
   ngOnInit() {
